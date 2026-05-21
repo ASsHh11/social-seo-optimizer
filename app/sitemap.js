d3a5514cb12429cd623b23a1db35f84c43cfa5ff
ASsHh11 <110763962+ASsHh11@users.noreply.github.com>
2026-05-21 07:21:52 +0500

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,9 +2,9 @@ export default async function sitemap() {
   return [
     {
       url: 'https://trendhack.vercel.app',
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(), // Date ko ISO string format mein bhejna zyada stable hota hai
       changeFrequency: 'daily',
-      priority: 1.0,
+      priority: 1,
     },
   ];
 }
